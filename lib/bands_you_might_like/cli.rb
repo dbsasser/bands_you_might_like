1c178f5d94cf0cfd5ef76ba35e622b6fd786c6d3
Daniel Sasser <danielbsasser@gmail.com>
2020-03-08 15:26:36 +0000

    Added Input Loop After Detailed Band View

--- lib/bands_you_might_like/cli.rb
+++ lib/bands_you_might_like/cli.rb
@@ -10,6 +10,7 @@ class BYML
     BandScraper.make_bands
     self.list_similar_bands
     self.select_similar_band
+    self.loop_input
   end
 
   def self.splash
@@ -20,13 +21,13 @@ class BYML
     BBBBBB   YYYYY  MM MM MM LL
     BB   BB   YYY   MM    MM LL
     BBBBBB    YYY   MM    MM LLLLLLL
-
-    Welcome to Bands You Might Like.
     HEREDOC
+
+    puts "Welcome to Bands You Might Like. The goal of this program is to help you disover new music to listen to. By entering a band or artist that your currently like, we will search the web to find similar music. To get started follow the prompts below.\n".white
   end
 
   def self.get_input_band
-    puts "Enter your favorite band or artist to find similar bands/artists you might like:".light_blue
+    puts "Enter one of your favorite bands or artist to find others you might like:".light_blue
     @@input_band = gets.strip.downcase
   end
 
@@ -65,6 +66,25 @@ class BYML
       self.select_similar_band
     end
   end
+
+  def self.loop_input
+    puts "What would you like to do now? Type 'back' to return to the list, 'search' to begin a new search, or 'exit' to leave the program"
+    input = gets.strip.downcase
+    case input
+    when "back"
+      self.list_similar_bands
+      self.select_similar_band
+    when "search"
+      SimilarBands.all.clear
+      self.call
+    when "exit"
+      self.goodbye
+    else
+      puts "Sorry, I don't understand."
+      self.loop_input
+    end
+  end
+
 
   def self.goodbye
     puts "See you next time. We hope you discovered some new bands to listen to."
