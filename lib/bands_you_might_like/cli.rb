f067855371a107bc31beae7efd4b10d0f72b5f20
Daniel Sasser <danielbsasser@gmail.com>
2020-03-08 15:04:04 +0000

    Added Spacing and Terminal Colors

--- lib/bands_you_might_like/cli.rb
+++ lib/bands_you_might_like/cli.rb
@@ -1,53 +1,57 @@
 class BYML
-  
-  def initialize 
-    
+
+  def initialize
+
   end
-  
+
   def self.call
-    self.greeting
     self.get_input_band
     BandScraper.get_page
     BandScraper.make_bands
     self.list_similar_bands
     self.select_similar_band
   end
-  
-  def self.greeting
-    puts <<~HEREDOC.yellow 
-    
-    BBBBB   YY   YY MM    MM LL      
-    BB   B  YY   YY MMM  MMM LL      
-    BBBBBB   YYYYY  MM MM MM LL      
-    BB   BB   YYY   MM    MM LL      
-    BBBBBB    YYY   MM    MM LLLLLLL 
-    
-    
+
+  def self.splash
+    puts <<~HEREDOC.yellow
+
+    BBBBB   YY   YY MM    MM LL
+    BB   B  YY   YY MMM  MMM LL
+    BBBBBB   YYYYY  MM MM MM LL
+    BB   BB   YYY   MM    MM LL
+    BBBBBB    YYY   MM    MM LLLLLLL
+
+    Welcome to Bands You Might Like.
     HEREDOC
-    
-    puts "Enter your favorite band or artist to find similar bands/artists you might like:"
   end
-  
+
   def self.get_input_band
+    puts "Enter your favorite band or artist to find similar bands/artists you might like:".light_blue
     @@input_band = gets.strip.downcase
   end
-  
+
   def self.convert_input_to_param
     "#{@@input_band.gsub(" ","+")}/+similar"
   end
-    
+
   def self.list_similar_bands
-    puts "Similar Bands:"
+    puts "\nSimilar Bands:\n\n"
     SimilarBands.all.each.with_index(1) do |band, index|
-      puts "#{index}. #{band.name.upcase} - #{band.genres.join(", ")} - Popularity: #{band.popularity}".colorize(:color => :yellow)
+      print "#{index}. ".white
+      print "#{band.name}".yellow.bold
+      print " - ".white
+      print "#{band.genres.join(", ")}"
+      print " - "
+      print "Popularity: #{band.popularity}".magenta
+      puts "\n"
     end
   end
-  
+
   def self.select_similar_band
-    puts "Enter the number of a band you would like to learn more about and listen to. You can also type 'back' to search for another band or 'exit' to leave.".cyan
+    puts "\nEnter the number of a band you would like to learn more about and listen to. You can also type 'back' to search for another band or 'exit' to leave.".light_blue
     input = gets.strip.downcase
-    
-    
+
+
     if input.to_i > 0 && input.to_i < 7
       BandScraper.scrape_songs(SimilarBands.all[input.to_i - 1])
       SimilarBands.all[input.to_i - 1].detailed_view
@@ -57,11 +61,11 @@ class BYML
     elsif input == "exit"
       self.goodbye
     else
-      puts "Sorry, I don't understand." 
+      puts "Sorry, I don't understand."
       self.select_similar_band
     end
   end
-  
+
   def self.goodbye
     puts "See you next time. We hope you discovered some new bands to listen to."
   end
